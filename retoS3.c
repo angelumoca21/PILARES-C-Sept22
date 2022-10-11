d572239c1e85b2fee2baeda582dcb556a09fa744
angelumoca21 <angelmoca21@gmail.com>
2022-10-11 09:26:29 -0500

    reto3

---
+++ retoS3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("         _nnnn_\n");
+    printf("        dGGGGMMb\n");
+    printf("       @p~qp~~qMb\n");
+    printf("       M|@||@) M|\n");
+    printf("       @,----.JM|\n");
+    printf("      JS^\\__/  qKL\n");
+    printf("     dZP        qKRb\n");
+    printf("    dZP          qKKb\n");
+    printf("   fZP            SMMb\n");
+    printf("   HZM            MMMM\n");
+    printf("   FqM            MMMM\n");
+    printf(" __| \".        |\\dS\"qML\n");
+    printf(" |    `.       | `' \\Zq\n");
+    printf("_)      \\.___.,|     .'\n");
+    printf("\\____   )MMMMMP|   .'\n");
+    printf("     `-'       `--' hjm\n");
+}
+
